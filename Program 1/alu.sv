f71a9527cb4dc29607219f35bc7714e39f0a1e42
ethanlee7102 <109195042+ethanlee7102@users.noreply.github.com>
2024-06-13 22:34:43 -0700

    Update alu.sv

--- Program 1/alu.sv
+++ Program 1/alu.sv
@@ -28,7 +28,7 @@ always_comb begin
       end*/
     3'b010: // right shift (alternative syntax -- works like left shift
       {rslt,sc_o} = {sc_i,inA};
-    3'b011: // NAND
+    3'b011: // XOR
        rslt = (inA ^ inB);
     3'b100: // SUB
       {sc_o,rslt} = inB - inA + sc_i;
